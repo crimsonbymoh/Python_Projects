939c3fbfd10c2cf983ee1dcbf4b6f010d97aa92d
crimsonbymoh <165728018+crimsonbymoh@users.noreply.github.com>
2024-04-06 09:42:34 +0100

    Add files via upload

---
+++ TaskNine.py
@@ -0,0 +1,26 @@
+totalCounter = 1;
+multiple1 = 4;
+totalCount = 1;
+multiple2 = 8;
+sumCounter = 0;
+sumCount = 0;
+sumTotal = 0;
+squareOfSum = 0;
+
+for counter in range(5):
+	totalCounter = totalCounter * multiple1 
+
+	sumCounter = sumCounter + totalCounter
+
+for counter1 in range(5):
+
+	totalCount = totalCount * multiple2
+
+	sumCount = sumCount + totalCount
+
+sumTotal = sumCounter + sumCount
+
+squareOfSum = sumTotal * sumTotal
+
+print(squareOfSum, end= ' ')
+
