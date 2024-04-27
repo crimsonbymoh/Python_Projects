2dacb5f242dd81e46e562aec7c423101590c2a20
moh-Adedamola <165728018+moh-Adedamola@users.noreply.github.com>
2024-04-27 09:19:30 +0100

    Update test_repeatstring.py

--- test_repeatstring.py
+++ test_repeatstring.py
@@ -6,20 +6,11 @@ def test_repeat_string():
 
 	assert repeat_string("hello", 3) == "hellohellohello"
 	assert repeat_string("hi", 4.5) == "hi"
-	assert repeat_string("hi", 4.5) == "hihihihi"
+	assert repeat_string("hi", 4) == "hihihihi"
 	assert repeat_string("welcome", 3) == "welcomewelcomewelcome"
-	assert repeat_string("welcome", 2.5) == "welcomewelcomewelcome"
+	assert repeat_string("welcome", 2.5) == "welcome"
+	assert repeat_string("attitude", 7.5) == "attitude"
 	
 	
 	
 	
-	
-
-
-
-
-
-
-# Test cases
-print  # Output: 
-print(repeat_string)    # Output: 'hi'
