6c857b964c786f98576636f937ea04a5f646c083
moh-Adedamola <165728018+moh-Adedamola@users.noreply.github.com>
2024-04-27 09:26:34 +0100

    Update test_removeoddindex.py

--- test_removeoddindex.py
+++ test_removeoddindex.py
@@ -6,7 +6,11 @@ def test_remove_odd_index():
 	assert remove_odd_index("semicolon") == "eioo"
 	assert remove_odd_index("Mohammed") == "oamd"
 	assert remove_odd_index("Adedamola") == "ddml"
-	assert remove_odd_index("Adedotun") == "aeou"
+	assert remove_odd_index("Adedotun") == "ddtn"
+	assert remove_odd_index("welcome") == "ecm"
+	assert remove_odd_index("Agreement") == "gemn"
+	assert remove_odd_index("pardon") == "adn"
+	assert remove_odd_index("coding") == "oig"
 	
 	
 
